d7c13ae74f93099a93dccf38ee9df2bb935e8291
Your Name <cer20005@byui.edu>
2020-11-28 00:02:36 -0300

    fishHaven

--- WDD230/lesson11/js/preston.js
+++ WDD230/lesson11/js/preston.js
@@ -97,5 +97,40 @@ fetch(requestURL)
                 document.querySelector('section.homeImages').appendChild(card);
             }
 
+
         }
+    });
+
+fetch(requestURL)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(jsonObject) {
+
+        const towns = jsonObject['towns'];
+
+        for (let i = 0; i < towns.length; i++) {
+
+            if (towns[i].name == "Fish Haven") {
+
+                let div = document.createElement('div');
+                let event1 = document.createElement('p');
+                let event2 = document.createElement('p');
+                let event3 = document.createElement('p');
+                let event4 = document.createElement('p');
+
+                event1.textContent = towns[i].events[0];
+                event2.textContent = towns[i].events[1];
+                event3.textContent = towns[i].events[2];
+                event4.textContent = towns[i].events[3];
+
+                div.appendChild(event1);
+                div.appendChild(event2);
+                div.appendChild(event3);
+                div.appendChild(event4);
+                document.querySelector('article.eventsFH').appendChild(div);
+            }
+
+        }
+
     });
