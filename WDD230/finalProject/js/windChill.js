e52f31665ad169783cc7a6f2e48dfade274e1fa5
Your Name <cer20005@byui.edu>
2020-11-30 15:44:12 -0300

    final

--- WDD230/finalProject/js/windChill.js
+++ WDD230/finalProject/js/windChill.js
@@ -1,6 +1,4 @@
-window.onload = windChill;
-
-function windChill() {
+window.addEventListener('DOMContentLoaded', (event) => {
     let currentTemp = document.getElementById('highTemp').innerHTML;
     let temp = Number(currentTemp);
 
@@ -9,9 +7,9 @@ function windChill() {
 
 
     if (temp <= 50 && wind > 3) {
-        let windChill = 35.74 + (0.6215 * temp) - (35.75 * wind ** 0.16) + (0.4275 * temp * wind ** 0.16);
-        document.getElementById('windCh').innerHTML = Math.round(windChill) + "°F";
+        let windChillCalc = 35.74 + (0.6215 * temp) - (35.75 * wind ** 0.16) + (0.4275 * temp * wind ** 0.16);
+        document.getElementById('windCh').innerHTML = Math.round(windChillCalc) + "°F";
     } else {
         document.getElementById('windCh').innerHTML = "N/A";
     }
-}
+})
