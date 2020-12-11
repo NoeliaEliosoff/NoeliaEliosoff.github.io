40cca3f0b002dda8dd62b7f4e9241c21de5d2cf1
Your Name <cer20005@byui.edu>
2020-12-11 02:12:44 -0300

    week13

---
+++ WDD230/finalPScoots/js/rentals.js
@@ -0,0 +1,19 @@
+const rentURL = 'data/rentals.json';
+
+fetch(rentURL)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(jsonObject) {
+        const types = jsonObject['types'];
+
+        for (let i = 0; i < types.length; i++) {
+            document.getElementById(`rT${i+1}`).textContent = types[i].rentalType;
+            document.getElementById(`mP${i+1}`).textContent = types[i].maxPersons;
+            document.getElementById(`hDR${i+1}`).textContent = types[i].halfDayRes;
+            document.getElementById(`fDR${i+1}`).textContent = types[i].fullDayRes;
+            document.getElementById(`hD${i+1}`).textContent = types[i].halfDayWI;
+            document.getElementById(`fD${i+1}`).textContent = types[i].fullDayWI;
+
+        }
+    });
